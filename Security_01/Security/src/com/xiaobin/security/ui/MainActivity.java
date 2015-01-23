5d86bae2e04f9c80422f90f1a0a92b1cba561230
taotaoma456 <531227630@qq.com>
2015-01-23 21:04:06 +0800

    手机卫士01
    手机卫士01

---
+++ Security_01/Security/src/com/xiaobin/security/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xiaobin.security.ui;
+
+import com.xiaobin.security.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
